e3b25288a9818ef2441c15bd048cb4e59e8e8e48
aidazi <biosubj@gmail.com>
2026-05-18 11:34:34 +0100

    #

---
+++ src/main/java/com/semantyca/datanest/model/cnst/ApprovalStatus.java
@@ -0,0 +1,29 @@
+package com.semantyca.datanest.model.cnst;
+
+import com.semantyca.mixpla.model.cnst.QueuePriority;
+
+public enum ApprovalStatus {
+    OPEN(500),
+    CANCELLED(501),
+    REJECTED_NOT_MEET_GENRE(502),
+    REJECTED(503),
+    ACCEPTED(505);
+
+    private final int value;
+
+    ApprovalStatus(int value) {
+        this.value = value;
+    }
+
+    public int value() {
+        return value;
+    }
+
+    public static ApprovalStatus fromValue(Integer v) {
+        if (v == null) return null;
+        for (ApprovalStatus p : values()) {
+            if (p.value == v) return p;
+        }
+        return null;
+    }
+}
