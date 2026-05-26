331868e6e6730076018c3224cf6a7f7cf17d1280
aidazi <biosubj@gmail.com>
2026-05-26 19:26:29 +0100

    revert: remove custom deserializer from StagePlaylistDTO

--- src/main/java/com/semantyca/datanest/dto/StagePlaylistDTO.java
+++ src/main/java/com/semantyca/datanest/dto/StagePlaylistDTO.java
@@ -1,15 +1,10 @@
 package com.semantyca.datanest.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.semantyca.datanest.dto.script.ScenePromptDTO;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
@@ -17,22 +12,7 @@ import java.util.UUID;
 @Getter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class StagePlaylistDTO {
-    @JsonDeserialize(using = StringOrFirstArrayDeserializer.class)
     private String sourcing;
-
-    static class StringOrFirstArrayDeserializer extends JsonDeserializer<String> {
-        @Override
-        public String deserialize(JsonParser p, DeserializationContext ctx) throws IOException {
-            if (p.isExpectedStartArrayToken()) {
-                String first = null;
-                while (p.nextToken() != com.fasterxml.jackson.core.JsonToken.END_ARRAY) {
-                    if (first == null) first = p.getText();
-                }
-                return first;
-            }
-            return p.getText();
-        }
-    }
     private String title;
     private String artist;
     private List<UUID> genres;
